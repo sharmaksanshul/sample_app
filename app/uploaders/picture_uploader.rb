b2e7017f6f6f1891f6dac49e218c0d9e2af5ab61
anshul sharma <anshuksharma12>
2019-06-10 16:17:01 +0530

    changed storage as file in carrierwave

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,9 +8,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # if Rails.env.production?
   #   storage :fog
   # else
-  #   storage :file
+     storage :file
   # end
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
